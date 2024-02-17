d552b11a7df35235a86ad21c06d5f67659b0b8b8
James Frost <git@frost.cx>
2024-02-17 15:51:45 +0000

    Make login work

--- backend/server.py
+++ backend/server.py
@@ -10,8 +10,9 @@ import stuff
 db = sqlite3.connect("db.sqlite3")
 db.execute("CREATE TABLE IF NOT EXISTS users(user_id INT, name TEXT)")
 db.execute(
-    "CREATE TABLE IF NOT EXISTS scribbles(user_id INT, name TEXT, image BLOB, attack INT, defence INT, speed INT, health INT, type: TEXT, type2: TEXT)"
+    "CREATE TABLE IF NOT EXISTS scribbles(user_id INT, name TEXT, image BLOB, attack INT, defence INT, speed INT, health INT, type TEXT, type2 TEXT)"
 )
+del db
 
 app = flask.Flask(__name__)
 app.secret_key = b"*s\xd3\xea%\xc7\x99\x8e\xb1\x13V\rpG\xf3\x8c\xf6\xcb'J7f\xc3\xe1\xb7\r\xe2\xbe8\x1cH\xe8"
@@ -26,7 +27,9 @@ def index():
 
 @app.post("/api/login")
 def login():
+    db = sqlite3.connect("db.sqlite3")
     username = flask.request.json["username"]
+    print(username)
     user_id = stuff.get_user_id_from_username(db, username)
     if not user_id:
         user_id = stuff.create_user(db, username)
@@ -39,8 +42,10 @@ def login():
 @app.post("/api/logout")
 def logout():
     # Remove the username from the session if it's there
+    resp = flask.make_response(flask.redirect(flask.url_for("index")))
+    resp.set_cookie("username", expires=0)
     flask.session.pop("username", None)
-    return flask.redirect(flask.url_for("index"))
+    return resp
 
 
 @app.post("/api/create_scribble")
--- backend/stuff.py
+++ backend/stuff.py
@@ -1,10 +1,15 @@
 # Where processing logic lives.
 
+import sqlite3
+
 
 def create_user(database, username):
     """Create a new player."""
     pass
 
-def get_user_id_from_username(database, username):
+
+def get_user_id_from_username(database: sqlite3.Connection, username):
     """Get the user_id from a username."""
-    pass
+    return database.execute(
+        "SELECT user_id FROM users WHERE name = ?", (username,)
+    ).fetchone()
