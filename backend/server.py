4e5a1b56a4cbabd079033dd381b97e25e0b1f974
James Frost <git@frost.cx>
2024-02-17 14:43:50 +0000

    Add some methods

--- backend/server.py
+++ backend/server.py
@@ -1,35 +1,39 @@
 #! /usr/bin/env python3
 
-"""Doodlemon."""
+"""Scribble Scrap."""
 
-from flask import Flask, session, request, redirect, url_for
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.secret_key = b"*s\xd3\xea%\xc7\x99\x8e\xb1\x13V\rpG\xf3\x8c\xf6\xcb'J7f\xc3\xe1\xb7\r\xe2\xbe8\x1cH\xe8"
 
 
 @app.get("/")
 def index():
-    if "username" in session:
-        return f'Logged in as {session["username"]}'
+    if "username" in flask.session:
+        return f'Logged in as {flask.session["username"]}'
     return "You are not logged in"
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.post("/api/login")
 def login():
-    if request.method == "POST":
-        session["username"] = request.form["username"]
-        return redirect(url_for("index"))
-    return """
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    """
+    flask.session["username"] = flask.request.json["username"]
+    return flask.redirect(flask.url_for("index"))
 
 
-@app.route("/logout")
+@app.post("/api/logout")
 def logout():
-    # remove the username from the session if it's there
-    session.pop("username", None)
-    return redirect(url_for("index"))
+    # Remove the username from the session if it's there
+    flask.session.pop("username", None)
+    return flask.redirect(flask.url_for("index"))
+
+
+@app.post("/api/create_scribble")
+def create_scribble():
+    if "username" not in flask.session:
+        flask.abort(401)
+    return "Cool!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
