3a834de2ab6eb2657b4d1c5d9604c3d534ba5746
James Frost <git@frost.cx>
2024-02-17 15:30:57 +0000

    Add login

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,15 @@
 """Scribble Scrap."""
 
 import flask
+import sqlite3
+
+import stuff
+
+db = sqlite3.connect("db.sqlite3")
+db.execute("CREATE TABLE IF NOT EXISTS users(user_id INT, name TEXT)")
+db.execute(
+    "CREATE TABLE IF NOT EXISTS scribbles(user_id INT, name TEXT, image BLOB, attack INT, defence INT, speed INT, health INT, type: TEXT, type2: TEXT)"
+)
 
 app = flask.Flask(__name__)
 app.secret_key = b"*s\xd3\xea%\xc7\x99\x8e\xb1\x13V\rpG\xf3\x8c\xf6\xcb'J7f\xc3\xe1\xb7\r\xe2\xbe8\x1cH\xe8"
@@ -17,8 +26,14 @@ def index():
 
 @app.post("/api/login")
 def login():
-    flask.session["username"] = flask.request.json["username"]
-    return flask.redirect(flask.url_for("index"))
+    username = flask.request.json["username"]
+    user_id = stuff.get_user_id_from_username(db, username)
+    if not user_id:
+        user_id = stuff.create_user(db, username)
+    flask.session["user_id"] = user_id
+    resp = flask.make_response()
+    resp.set_cookie("username", flask.request.json["username"])
+    return resp
 
 
 @app.post("/api/logout")
---
+++ backend/stuff.py
@@ -0,0 +1,10 @@
+# Where processing logic lives.
+
+
+def create_user(database, username):
+    """Create a new player."""
+    pass
+
+def get_user_id_from_username(database, username):
+    """Get the user_id from a username."""
+    pass
