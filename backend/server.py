243a52fa52ce16aeba55bfd2f0be32520421cb57
James Frost <git@frost.cx>
2024-02-17 17:50:21 +0000

    make things work

--- backend/server.py
+++ backend/server.py
@@ -2,17 +2,20 @@
 
 """Scribble Scrap."""
 
+import tempfile
+import urllib.parse
+
 import flask
-import sqlite3
+import redis
+import requests
 
-import stuff
 
-db = sqlite3.connect("db.sqlite3")
-db.execute("CREATE TABLE IF NOT EXISTS users(user_id INT, username TEXT)")
-db.execute(
-    "CREATE TABLE IF NOT EXISTS scribbles(user_id INT, name TEXT, image BLOB, attack INT, defence INT, speed INT, health INT, type TEXT, type2 TEXT)"
-)
-del db
+# Constants:
+
+IMAGE_CROPPER_URL = "http://localhost:7001"
+
+
+r = redis.Redis()
 
 app = flask.Flask(__name__)
 app.secret_key = b"*s\xd3\xea%\xc7\x99\x8e\xb1\x13V\rpG\xf3\x8c\xf6\xcb'J7f\xc3\xe1\xb7\r\xe2\xbe8\x1cH\xe8"
@@ -27,12 +30,13 @@ def index():
 
 @app.post("/api/login")
 def login():
-    db = sqlite3.connect("db.sqlite3")
     username = flask.request.json["username"]
-    print(username)
-    user_id = stuff.get_user_id_from_username(db, username)
-    if not user_id:
-        user_id = stuff.create_user(db, username)
+    user_id = r.get(f"user_id:{username}")
+    if user_id is None:
+        print("Creating account for", username)
+        user_id = username
+        r.set(f"user_id:{username}", username)
+    print(username, "logged in.")
     flask.session["user_id"] = user_id
     resp = flask.make_response()
     resp.set_cookie("username", flask.request.json["username"])
@@ -52,7 +56,16 @@ def logout():
 def create_scribble():
     if "username" not in flask.session:
         flask.abort(401)
-    return "Cool!"
+    username = flask.session["username"]
+    user_id = r.get(f"user_id:{username}")
+
+    image_path = tempfile.mktemp(".jpg")
+    flask.request.files[0].save(image_path)
+    r.set(f"{user_id}:raw_mon", image_path)
+    # Request to cropping server.
+    requests.get(IMAGE_CROPPER_URL + f"?path={urllib.parse.quote_plus(image_path)}")
+
+    return "OK"
 
 
 if __name__ == "__main__":
